4ceecdeeb6b0dd340ec3bdefeee4d1dad4747f8f
Gustavo Garone <gustavogarone@protonmail.com>
2025-12-01 13:56:11 -0800

    feat: use 1pass

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,5 +1,10 @@
 {inputs, ...}: {
   programs.firefox = {
     enable = true;
+    policies = {
+      Extensions = {
+        Install = ["https://addons.mozilla.org/firefox/downloads/1password.xpi"];
+      };
+    };
   };
 }
