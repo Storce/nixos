8c9418786ec510e5d525973864a91e6aa9e724e4
storce <zezhou_wang@berkeley.edu>
2025-12-02 13:01:12 -0800

    feat: change hostname

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -1,5 +1,5 @@
 {
-  networking.hostName = "den"; # Define your hostname.
+  networking.hostName = "artanis"; # Define your hostname.
   # Pick only one of the below networking options.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
